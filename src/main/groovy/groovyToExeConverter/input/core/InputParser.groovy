b92bb494f293b3cd8c21d4d777322177ade75ca4
Todd Elvers <todd.elvers@gmail.com>
2014-03-10 22:15:51 -0500

    Usage message clarification

--- src/main/groovy/groovyToExeConverter/input/core/InputParser.groovy
+++ src/main/groovy/groovyToExeConverter/input/core/InputParser.groovy
@@ -16,7 +16,7 @@ class InputParser {
             f(longOpt: 'fileToConvert', args: 1, argName: 'file', 'Required. The groovy script or jar file to convert to an exe. (If issues occur, use quotes around path)')
             d(longOpt: 'destDir', args: 1, argName: 'directory', "Optional. Executable destination directory.\n(Default = fileToConvert's parent dir)")
             t(longOpt: 'tempDir', args: 1, argName: 'directory', "Optional. Overrides the directory where temporary files are written to. Files will be written to a folder entitled 'g2exe' in the given directory. \n(Default = <The-System's-Temp>/g2exe)")
-            m(longOpt: 'minJre', args: 1, argName: 'jre_version', "Optional. The minimum JRE the application will accept when searching a user's system for a JRE. Accepts values like 'x.x.x' or 'x.x.xx'.\n(Default = '1.5.0')")
+            m(longOpt: 'minJre', args: 1, argName: 'jre_version', "Optional. The minimum JRE the application will accept when searching a user's system for a JRE. Accepts values like 'x.x.x' or 'x.x.xx'.\n(Default = 1.5.0)")
             i(longOpt: 'icon', args: 1, argName: 'file', "Optional.  Fully qualified path to an .ico file that will be used as the file's icon.\n(Default = g2exe's icon)")
             xms(longOpt: 'initHeapSize', args: 1, argName: 'size', "Optional. The JVM's initial heap size in MB.\n(Default = 128)")
             xmx(longOpt: 'maxHeapSize', args: 1, argName: 'size', "Optional. The JVM's maximum heap size in MB.\n(Default = 512)")
