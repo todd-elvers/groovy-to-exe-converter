a454d827c879649a39ee167654a41955fed4b03a
Todd Elvers <todd.elvers@gmail.com>
2014-03-30 19:51:59 -0500

    added some TODOs for future reference

--- src/main/groovy/groovyToExeConverter/core/groovyToJar/GroovyToJarFileConverter.groovy
+++ src/main/groovy/groovyToExeConverter/core/groovyToJar/GroovyToJarFileConverter.groovy
@@ -3,10 +3,8 @@ package groovyToExeConverter.core.groovyToJar
 import groovy.transform.CompileStatic
 import groovy.util.logging.Log4j
 import groovyToExeConverter.core.FileConverter
+import groovyToExeConverter.core.groovyToJar.core.GroovyScriptHandler
 import groovyToExeConverter.core.groovyToJar.core.JarBuilder
-
-import static groovyToExeConverter.core.groovyToJar.core.GroovyScriptHandler.compileScriptToClassFilesInSameDirAndLoadIntoMemory
-import static groovyToExeConverter.core.groovyToJar.core.GroovyScriptHandler.findNameOfClassWithMainMethod
 
 @Log4j
 @CompileStatic
@@ -17,8 +15,8 @@ class GroovyToJarFileConverter extends FileConverter {
         log.info("${appConfig.fileToConvert.name} --> ${appConfig.jarFileName}")
 
         final script = resourceHandler.copyFileToTempDir(appConfig.fileToConvert)
-        final loadedScript = compileScriptToClassFilesInSameDirAndLoadIntoMemory(script)
-        final mainClassName = findNameOfClassWithMainMethod(loadedScript)
+        final loadedScript = GroovyScriptHandler.compileScriptToClassFilesInSameDirAndLoadIntoMemory(script)
+        final mainClassName = GroovyScriptHandler.findNameOfClassWithMainMethod(loadedScript)
         final jarFile = resourceHandler.createFileInTempDir(appConfig.jarFileName)
 
         new JarBuilder(mainClass: mainClassName, destFile: jarFile).build()
