fc2cd110f0c4e1cf40bf31bfa6f2ab5f7d721f91
Todd Elvers <todd.elvers@gmail.com>
2014-04-14 19:24:54 -0500

    BugFix: JreVersionValidator now only allow versions in the format of x.x.x[_xx] (which is what Launch4j accepts)

--- src/main/groovy/groovyToExeConverter/input/core/parameterValidators/JreVersionValidator.groovy
+++ src/main/groovy/groovyToExeConverter/input/core/parameterValidators/JreVersionValidator.groovy
@@ -1,13 +1,11 @@
 package groovyToExeConverter.input.core.parameterValidators
 
-import groovy.util.OptionAccessor as Input
-
 class JreVersionValidator {
 
-    // Only digits and periods in the form: 'x.x.x' or 'x.x.xx'
-    static final JRE_VERSION_PATTERN = ~/^\d{1}.\d{1}.\d{1,2}$/
+    // Only digits and periods in the form: 'x.x.x[_xx]'
+    static final JRE_VERSION_PATTERN = ~/^\d.\d.\d(_\d\d){0,1}$/
 
-    static def minJreInInvalidFormat = { Input input ->
+    static def minJreInInvalidFormat = { input ->
         if (!input.minJre) return false
 
         String minJre = input.minJre
---
+++ src/test/groovy/groovyToExeConverter/input/core/parameterValidators/JreVersionValidatorUnitTest.groovy
@@ -0,0 +1,26 @@
+package groovyToExeConverter.input.core.parameterValidators
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class JreVersionValidatorUnitTest extends Specification {
+
+    @Unroll
+    def "given invalid JRE version #jreVersion, minJreInInvalidFormat() returns true"() {
+        expect:
+            JreVersionValidator.minJreInInvalidFormat(new Expando([minJre: jreVersion]))
+
+        where:
+            jreVersion << ['1.4.00', '1.2', '1', '1.5.0_245']
+    }
+
+    @Unroll
+    def "given JRE Version #jreVersion, minJreInInvalidFormat() returns false"() {
+        expect:
+            !JreVersionValidator.minJreInInvalidFormat(new Expando([minJre: jreVersion]))
+
+        where:
+            jreVersion << ['1.4.0', '1.5.1', '1.5.0_24']
+    }
+
+}
