c0ba921a0ebd6f18ec9bb6124655f34a924cd11b
todd-elvers <todd.elvers@gmail.com>
2018-04-09 13:06:35 -0500

    Fixed issue #10 - upgraded to Launch4j 3.11

--- src/main/groovy/groovyToExeConverter/model/ResourceFileNames.groovy
+++ src/main/groovy/groovyToExeConverter/model/ResourceFileNames.groovy
@@ -2,6 +2,7 @@ package groovyToExeConverter.model
 
 import groovy.transform.TupleConstructor
 
+//TODO: Change this to something simpler.  Maybe store Launch4j as ZIP inside JAR and extract it.
 @TupleConstructor
 enum ResourceFileNames {
     G2EXE_RESOURCES(["gradle.properties", "g2exeIcon.ico"]),
@@ -9,6 +10,7 @@ enum ResourceFileNames {
             'launch4j/launch4j.exe',
             'launch4j/launch4jc.exe',
             'launch4j/launch4j.jar',
+
             'launch4j/lib/ant.jar',
             'launch4j/lib/commons-beanutils.jar',
             'launch4j/lib/commons-logging.jar',
@@ -18,16 +20,26 @@ enum ResourceFileNames {
             'launch4j/lib/jgoodies-forms.jar',
             'launch4j/lib/jgoodies-looks.jar',
             'launch4j/lib/xstream.jar',
+
             'launch4j/w32api/crt2.o',
             'launch4j/w32api/libadvapi32.a',
             'launch4j/w32api/libgcc.a',
             'launch4j/w32api/libkernel32.a',
             'launch4j/w32api/libmingw32.a',
-            'launch4j/w32api/libmingwex.a',
-            'launch4j/w32api/libmoldname.a',
             'launch4j/w32api/libmsvcrt.a',
             'launch4j/w32api/libshell32.a',
             'launch4j/w32api/libuser32.a',
+
+            'launch4j/w32api_jni/crt2.o',
+            'launch4j/w32api_jni/libadvapi32.a',
+            'launch4j/w32api_jni/libgcc.a',
+            'launch4j/w32api_jni/libkernel32.a',
+            'launch4j/w32api_jni/libmingw32.a',
+            'launch4j/w32api_jni/libmingwex.a',
+            'launch4j/w32api_jni/libmoldname.a',
+            'launch4j/w32api_jni/libmsvcrt.a',
+            'launch4j/w32api_jni/libshell32.a',
+            'launch4j/w32api_jni/libuser32.a',
 
             // TODO: See where/how these are used in launch4j.  Maybe we don't need them till we add a feature to use them w/ launch4j?
             'launch4j/sign4j/sign4j.c',
@@ -36,6 +48,7 @@ enum ResourceFileNames {
             'launch4j/head/consolehead.o',
             'launch4j/head/guihead.o',
             'launch4j/head/head.o',
+
             'launch4j/bin/ld.exe',
             'launch4j/bin/windres.exe'
     ])
